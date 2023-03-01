6a632e8141993c79800d48a80f21043b6ef7cfa7
Simeon Yordanov <lookmanche@gmail.com>
2023-03-01 22:50:47 +0200

    written tests

---
+++ CustomImplementation/CustomImplementation.Test/CustomStackTest.cs
@@ -0,0 +1,98 @@
+﻿namespace CustomImplementation.Test
+{
+    public class CustomStackTest
+    {
+        [Fact]
+        public void Push_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(1);
+
+            Assert.Equal(1, stack.Count);
+        }
+
+        [Fact]
+        public void Pop_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(1);
+
+            var value = stack.Pop();
+
+            Assert.Equal(1, value);
+        }
+
+        [Fact]
+        public void Pop_Should_Throw_InvalidOperationException_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [Fact]
+        public void Pop_With_Multiple_Values_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(1);
+
+            Assert.Equal(1, stack.Pop());
+            Assert.Equal(2, stack.Count);
+        }
+
+        [Fact]
+        public void Peek_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(1);
+
+            Assert.Equal(1, stack.Peek());
+            Assert.Equal(1, stack.Count);
+        }
+
+        [Fact]
+        public void Peek_With_Multiple_Values_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(1);
+
+            Assert.Equal(1, stack.Peek());
+            Assert.Equal(3, stack.Count);
+        }
+
+        [Fact]
+        public void Peek_Should_Throw_ArgumentNullException_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            Assert.Throws<ArgumentNullException>(() => stack.Peek());
+        }
+
+        [Fact]
+        public void Contains_Should_Return_True_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(2);
+
+            Assert.True(stack.Contains(2));
+        }
+
+        [Fact]
+        public void Contains_Should_Return_False_Test()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Push(2);
+
+            Assert.False(stack.Contains(3));
+        }
+    }
+}
---
+++ CustomImplementation/CustomImplementation/CustomStack.cs
@@ -0,0 +1,76 @@
+﻿namespace CustomImplementation
+{
+    public class CustomStack<ItemType> 
+    {
+        /// <summary>
+        /// field of type CustomLinkedList that conatains the items for
+        /// CustomStack
+        /// </summary>
+        private CustomLinkedList<ItemType> linkedList = new CustomLinkedList<ItemType>();
+
+        /// <summary>
+        /// The count of the sequence in the <see cref="CustomStack{ItemType}"/>
+        /// </summary>
+        public int Count => this.linkedList.Count;
+
+        /// <summary>
+        /// Adds an item in the sequence of the <see cref="CustomStack{ItemType}"/>
+        /// </summary>
+        /// <param name="item">The item that will be stored in the sequence of the <see cref="CustomStack{ItemType}"/></param>
+        public void Push(ItemType item)
+        {
+            linkedList.AddFirst(item);
+        }
+
+        /// <summary>
+        /// Removes an item from the sequence of the <see cref="CustomStack{ItemType}"/>
+        /// </summary>
+        /// <returns>Returns the item that is going to be removed</returns>
+        /// <exception cref="InvalidOperationException">If the sequence is empty</exception>
+        public ItemType Pop()
+        {
+            if (linkedList.Count == 0)
+            {
+                throw new InvalidOperationException("The sequence is empty");
+            }
+
+            return linkedList.RemoveFirst();
+        }
+
+        /// <summary>
+        /// Shows the first item in the sequence of the <see cref="CustomStack{ItemType}"/>.
+        /// This method does not remove the item from the sequence
+        /// </summary>
+        /// <returns>Returns item of type <see cref="{ItemType}"/></returns>
+        /// <exception cref="ArgumentNullException">Thrwos exception if the sequence is empty</exception>
+        public ItemType Peek()
+        {
+            if (linkedList.First == null)
+            {
+                throw new ArgumentNullException("They are no items in the sequence");
+            }
+
+            return linkedList.First.Value;
+        }
+
+        /// <summary>
+        /// Checks if sequence contains the <paramref name="item"/>
+        /// </summary>
+        /// <param name="item">item of type </param>
+        /// <returns></returns>
+        public bool Contains(ItemType item)
+        {
+            var array = this.linkedList.ToArray();
+
+            foreach (var element in array)
+            {
+                if (element.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
