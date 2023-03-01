d8ecde069eb5052a5b989e34a5f1ea336793c35b
Simeon Yordanov <lookmanche@gmail.com>
2023-03-01 22:49:53 +0200

    minor commit

--- CustomImplementation/CustomImplementation/Program.cs
+++ CustomImplementation/CustomImplementation/Program.cs
@@ -11,3 +11,12 @@ list.AddLast(1000);
 list.AddFirst(2000);
 Console.WriteLine(list.RemoveFirst());
 Console.WriteLine(list.RemoveLast());
+Console.WriteLine(list.Count);
+list.ForEach(x => Console.WriteLine(x.Value));
+var arr = list.ToArray();
+Console.WriteLine(String.Join("-", arr));
+
+CustomStack<int> test = new CustomStack<int>();
+Stack<int> test2 = new Stack<int>();
+test.Push(2);
+test.Push(1);
