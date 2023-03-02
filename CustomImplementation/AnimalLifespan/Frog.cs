70a7c740aba2a6fb508c9991d91298f25d1a05c8
Simeon Yordanov <lookmanche@gmail.com>
2023-03-02 21:54:29 +0200

    created Frog

---
+++ CustomImplementation/AnimalLifespan/Frog.cs
@@ -0,0 +1,23 @@
+﻿namespace AnimalLifespan
+{
+    public class Frog : Animal
+    {
+        public Frog()
+        {
+            this.Energy = this.MaxEnergy;
+
+            this.Diet = new List<Food>()
+            {
+                Food.Insects,
+                Food.Snails ,
+                Food.Slugs ,
+            };
+
+            Console.WriteLine($"{nameof(Frog)} starts with => {this.MaxEnergy} and his died is {string.Join(", ", this.Diet)}");
+        }
+
+        public override IReadOnlyList<Food> Diet { get; }
+
+        public override int MaxEnergy => 4;
+    }
+}
