ee158db4e579f4d104b6885bde98264fbac3063f
Simeon Yordanov <lookmanche@gmail.com>
2023-03-02 21:53:51 +0200

    created for loop for feeding the animals with a random food

--- CustomImplementation/AnimalLifespan/Program.cs
+++ CustomImplementation/AnimalLifespan/Program.cs
@@ -1,4 +1,28 @@
 ﻿using AnimalLifespan;
 
-var rabit = new Rabbit(3);
+var rabbit = new Rabbit();
+var wolf = new Wolf();
+var fish = new Fish();
+var frog = new Frog();
 
+var random = new Random();
+
+Console.WriteLine();
+
+for (int i = 0; i < 5; i++)
+{
+    int foodForRabbit = random.Next(1, 12);
+    int foodForWolf = random.Next(1, 12);
+    int foodForFish = random.Next(1, 12);
+    int foodForFrog = random.Next(1, 12);
+
+    rabbit.Feed((Food)foodForRabbit);
+
+    wolf.Feed((Food)foodForWolf);
+
+    fish.Feed((Food)foodForFish);
+
+    frog.Feed((Food)foodForFrog);
+
+    Console.WriteLine(new String('=', 50));
+}
