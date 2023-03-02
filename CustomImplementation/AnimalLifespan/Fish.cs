2f3811d95eddeae8d8a539063abcf68e30213bd6
Simeon Yordanov <lookmanche@gmail.com>
2023-03-02 21:54:13 +0200

    created Fish

---
+++ CustomImplementation/AnimalLifespan/Fish.cs
@@ -0,0 +1,23 @@
+﻿namespace AnimalLifespan
+{
+    public class Fish : Animal
+    {
+        public Fish()
+        {
+            this.Energy = this.MaxEnergy;
+
+            this.Diet = new List<Food>()
+            {
+                Food.Algae,
+                Food.Worms,
+                Food.Plankton,
+            };
+
+            Console.WriteLine($"{nameof(Fish)} starts with => {this.MaxEnergy} and his died is {string.Join(", ", this.Diet)}");
+        }
+
+        public override IReadOnlyList<Food> Diet { get; }
+
+        public override int MaxEnergy => 2;
+    }
+}
