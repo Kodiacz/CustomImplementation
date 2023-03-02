049c5d14640a3b5801af9239d7d532a940afda23
Simeon Yordanov <lookmanche@gmail.com>
2023-03-02 21:54:40 +0200

    created Wolf

---
+++ CustomImplementation/AnimalLifespan/Wolf.cs
@@ -0,0 +1,21 @@
+﻿namespace AnimalLifespan
+{
+    public class Wolf : Animal
+    {
+        public Wolf()
+        {
+            this.Energy = this.MaxEnergy;
+
+            this.Diet = new List<Food>()
+            {
+                Food.RawMeat,
+            };
+
+            Console.WriteLine($"{nameof(Wolf)} starts with => {this.MaxEnergy} and his died is {string.Join(", ", this.Diet)}");
+        }
+
+        public override IReadOnlyList<Food> Diet { get; }
+
+        public override int MaxEnergy => 8;
+    }
+}
