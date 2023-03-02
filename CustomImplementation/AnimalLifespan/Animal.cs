2ecb6b036c2066a04f54f041a24d86fa0b4e8498
Simeon Yordanov <lookmanche@gmail.com>
2023-03-02 21:52:05 +0200

    the logic for Feed method was moved into Animal abstrac class

--- CustomImplementation/AnimalLifespan/Animal.cs
+++ CustomImplementation/AnimalLifespan/Animal.cs
@@ -2,10 +2,39 @@
 {
     public abstract class Animal
     {
-        public abstract int Energy { get; }
+        public abstract int MaxEnergy { get; }
+
+        public virtual int Energy { get; protected set; }
 
         public abstract IReadOnlyCollection<Food> Diet { get; }
 
-        public abstract void Feed(Food food);
+        public void Feed(Food food)
+        {
+            if (this.Diet.Contains(food) && this.Energy > 0)
+            {
+                if (this.Energy + 1 <= this.MaxEnergy)
+                {
+                    this.Energy++;
+                }
+            }
+            else
+            {
+                if (this.Energy > 0)
+                {
+                    this.Energy--;
+
+                    if (this.Energy == 0)
+                    {
+                        Console.WriteLine($"The {this.GetType().Name} died");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"The {this.GetType().Name} is dead");
+                }
+            }
+
+            Console.WriteLine($"{this.GetType().Name} was fed by {food} => {this.Energy}");
+        }
     }
 }
